02bdc9e682787d4fa3b4409482d0ca8a4c04885c
Aman <aluqman@hawk.iit.edu>
2023-03-03 14:32:28 -0600

    Revamped configs

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,7 @@
 import Config
 
 config :nostrum,
-  token: "nope",
-  gateway_intents: []
+  token: "#{System.get_env("TEST_BOT_TOKEN", "")}",
+  gateway_intents: :all
 
 if File.exists?("config/secrets.exs"), do: import_config("secrets.exs")
-
-if File.exists?("#{Mix.env()}.exs"), do: import_config("#{Mix.env()}.exs")
--- config/test.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-config :nostrum,
-  token: System.get_env("TEST_BOT_TOKEN", ""),
-  gateway_intents: :all
