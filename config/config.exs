5647b102c85f46e564f5c26c10fa353eb534da80
Aman <aluqman@hawk.iit.edu>
2023-03-03 14:15:27 -0600

    Optionally use test config

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,6 @@ config :nostrum,
   token: "nope",
   gateway_intents: []
 
-import_config "#{Mix.env()}.exs"
-
 if File.exists?("config/secrets.exs"), do: import_config("secrets.exs")
+
+if File.exists?("#{Mix.env()}.exs"), do: import_config("#{Mix.env()}.exs")
