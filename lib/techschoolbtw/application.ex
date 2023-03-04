eedfd0267ea0890df807b343e1d27d4ff60bbca8
Aman <aluqman@hawk.iit.edu>
2023-03-04 14:22:58 -0600

    Formatting

--- lib/techschoolbtw/application.ex
+++ lib/techschoolbtw/application.ex
@@ -7,9 +7,9 @@ defmodule Techschoolbtw.Application do
 
   @impl true
   def start(_type, _args) do
-    children = 
-      for n <- 1..System.schedulers_online(), do:
-        Supervisor.child_spec(Techschoolbtw.Consumer, id: {:techschoolbtw, :consumer, n})
+    children =
+      for n <- 1..System.schedulers_online(),
+          do: Supervisor.child_spec(Techschoolbtw.Consumer, id: {:techschoolbtw, :consumer, n})
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
