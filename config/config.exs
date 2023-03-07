b53e273676124c2c74361aa6db82a7eae798fa9d
Aman <aluqman@hawk.iit.edu>
2023-03-07 15:13:37 -0600

    Allow for retrieval of owner ID from environment

--- config/config.exs
+++ config/config.exs
@@ -4,4 +4,7 @@ config :nostrum,
   token: "#{System.get_env("TEST_BOT_TOKEN", "")}",
   gateway_intents: :all
 
+config :techschoolbtw,
+  owner_id: System.get_env("OWNER_ID")
+
 if File.exists?("config/secrets.exs"), do: import_config("secrets.exs")
